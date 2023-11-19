6e48ba2af58a7a7ad890fbd6ecb8a09b1f915c33
tangxiaoxin <1302947749@qq.com>
2023-11-19 20:49:33 +0800

    README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # @astii/compare-version
+with it you can compare versions, get the next version and more
+
+## Install
+
+```typeScript
+npm install @astii/compare-version --save
+
+```
+
+or
+
+```typeScript
+yarn add @astii/compare-version
+```
+
+## Usage
+
+```typeScript
+import { compareVersions, getNextVersion } from '@astii/compare-version'
+compareVersions("1.0.0", "1.0.1") //-1
+compareVersions("1.0.0", "1.0.0") //0
+compareVersions("1.0.1", "1.0.0") //1
+const nextVersion = getNextVersion("1.0.0") //1.0.1
+```
--- rollup.config.js
+++ rollup.config.js
@@ -4,37 +4,53 @@ const babel = require("@rollup/plugin-babel");
 const { DEFAULT_EXTENSIONS } = require("@babel/core");
 const { terser } = require("rollup-plugin-terser");
 const env = process.env.NODE_ENV;
-module.exports = {
-  input: "./src/index.ts",
-  output: [
-    {
-      name: "compareVersions",
-      file: path.join(__dirname, "lib/index.js"),
-      format: "umd",
-    },
-    {
-      dir: path.join(__dirname, "es"),
-      format: "es",
-    },
-  ],
-  plugins: [
-    typescript({
-      tsconfig: "./tsconfig.json",
-    }),
-    babel({
-      babelHelpers: "runtime",
-      exclude: "node_modules/**",
-      extensions: [...DEFAULT_EXTENSIONS, ".ts"],
-    }),
-    env === "production"
-      ? terser({
+
+const plugins = () => {
+  return env === "production"
+    ? [
+        terser({
           compress: {
             pure_getters: true,
             unsafe: true,
             unsafe_comps: true,
             warnings: false,
           },
-        })
-      : void 0,
+        }),
+      ]
+    : void 0;
+};
+
+module.exports = {
+  input: "./src/index.ts",
+  output: [
+    {
+      dir: path.join(__dirname, "es"),
+      format: "es",
+      esModule: true,
+      exports: "named",
+    },
+    {
+      file: path.join(__dirname, "lib/index.js"),
+      format: "cjs",
+    },
+    {
+      name: "compareVersions",
+      file: path.join(__dirname, "dist/index.js"),
+      format: "umd",
+    },
+  ],
+  plugins: [
+    typescript({
+      tsconfig: "./tsconfig.json",
+      compilerOptions: {
+        declaration: false,
+      },
+    }),
+    babel({
+      babelHelpers: "runtime",
+      exclude: "node_modules/**",
+      extensions: [...DEFAULT_EXTENSIONS, ".ts"],
+    }),
+    ...plugins(),
   ],
 };
